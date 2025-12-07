1b209f734ac10b0c143b0384a157860b09abc362
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-12-07 21:52:17 +0530

    feat: Create new Home page component composed of various sections.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Herosection from '../components/Herosection'
 import HomeWidgets from '../components/HomeWidgets'
 import Websiteview from '../components/Websiteview'
+import WhoWeAre from '../components/WhoWeAre'
 
 export default function Home({ onJobClick, onViewAllJobs }) {
   return (
     <div className="bg-white">
       <Herosection />
+      <WhoWeAre />
       <HomeWidgets onJobClick={onJobClick} onViewAllJobs={onViewAllJobs} />
       <Websiteview />
     </div>
