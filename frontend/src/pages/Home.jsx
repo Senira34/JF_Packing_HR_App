c39330d6090b62c1261514bf968a4300d88569fb
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-12-09 02:08:15 +0530

    feat: Add initial Home and Login/Signup pages.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Herosection from '../components/Herosection'
 import HomeWidgets from '../components/HomeWidgets'
 import Websiteview from '../components/Websiteview'
 import WhoWeAre from '../components/WhoWeAre'
+import LatestCreations from '../components/LatestCreations'
 
 export default function Home({ onJobClick, onViewAllJobs }) {
   return (
     <div className="bg-white">
       <Herosection />
       <WhoWeAre />
+      <LatestCreations />
       <HomeWidgets onJobClick={onJobClick} onViewAllJobs={onViewAllJobs} />
       <Websiteview />
     </div>
